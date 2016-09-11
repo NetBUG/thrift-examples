3a37df599610449769ed0a52a37b2b2b27d18192
NetBUG <darikcr@gmail.com>
2016-09-11 08:30:53 -0700

    Proper instruction

--- generator.sh
+++ generator.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 thrift --gen php:server Example.thrift
 thrift --gen py Example.thrift
 
-echo Please go to gen-php/Example.php and clean up all namespace references:
-echo 1) first line (namespace Example)
-echo 2) all use Thrift\... calls below
-echo 3) all \Example\... used in the file below.
-echo If you did all these steps correctly, congratulations! The server should work!
+echo "Please go to gen-php/Example.php and clean up all namespace references:"
+echo "a) first line (namespace Example)"
+echo "b) all use Thrift\... calls below"
+echo "c) all \Example\... used in the file below."
+echo "If you did all these steps correctly, congratulations! The server should work!"
